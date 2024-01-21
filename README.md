5d943787bc68c6e27f680ffef6449ce685f5cd67
Dat <dat.lemindast@gmail.com>
2024-01-21 13:14:02 +0700

    change

--- README.md
+++
@@ -1,3 +0,0 @@
-# work_efficiency_management_system
-
-
--- android/app/src/main/kotlin/com/example/work_efficiency_management_system/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.work_efficiency_management_system
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
