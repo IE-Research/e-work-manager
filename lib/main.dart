9fd2f327190412d31b49b565b9b757b267a1d4dc
Dat <dat.lemindast@gmail.com>
2024-01-21 13:13:16 +0700

    change

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,57 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+class LoginFrameInit extends StatefulWidget {
+  const LoginFrameInit({Key? key}) : super(key: key);
+
+  @override
+  State<LoginFrameInit> createState() => _LoginFrameState();
+  
+}
+
+class _LoginFrameState extends State<LoginFrameInit>{
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(32),
+      child : Row(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text('Username:'),
+                    // Adding a simple text input bar (TextField) for the username
+                    SizedBox(width: 10), // Add some spacing
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter your username',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Password:'),
+                    // Adding a simple text input bar (TextField) for the username
+                    SizedBox(width: 10), // Add some spacing
+                    Expanded(
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Enter your Password',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -84,7 +135,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: const Text('Home'),
+        title: const Text('Sign In'),
         actions: const <Widget>[],
       ),
       body: Center(
@@ -108,41 +159,46 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Row(
               children: <Widget>[
-                Text('Username:'),
-                // Adding a simple text input bar (TextField) for the username
-                SizedBox(width: 10), // Add some spacing
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter your username',
+                Row(
+                  children: <Widget>[
+                    Text('Username:'),
+                    // Adding a simple text input bar (TextField) for the username
+                    SizedBox(width: 10), // Add some spacing
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter your username',
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Password:'),
+                    // Adding a simple text input bar (TextField) for the username
+                    SizedBox(width: 10), // Add some spacing
+                    Expanded(
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Enter your Password',
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-            const Row(
-              children: <Widget>[
-                Text('Password:'),
-                // Adding a simple text input bar (TextField) for the username
-                SizedBox(width: 10), // Add some spacing
-                Expanded(
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your Password',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              
+            ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
